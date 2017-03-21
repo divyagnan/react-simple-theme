621353f90e744a7e254f90fcaf0f49d84b74f8f4
Divyagnan <divyagnan@users.noreply.github.com>
2017-03-21 15:44:54 -0400

    test(add tests): add tests for injectThemes and withTheme
    finally add in some working tests for the two external facing components. tested to ensure they
    passed props, provided their props correctly, and adpated to change properly

---
+++ __fixtures__/themes.js
@@ -0,0 +1,19 @@
+const themes = {
+  theme1: {
+    color: 'black',
+    outer: 'aliceblue',
+    inner: 'lightblue',
+  },
+  theme2: {
+    color: 'white',
+    outer: 'crimson',
+    inner: 'lightcoral',
+  },
+  theme3: {
+    color: 'black',
+    outer: 'chartreuse',
+    inner: 'lightgreen',
+  },
+}
+
+export default themes
---
+++ __tests__/injectThemes.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { injectThemes } from '../src'
+import themes from '../__fixtures__/themes'
+import RootComponent from '../__fixtures__/RootComponent'
+
+describe('injectThemes', () => {
+  it('passes the correct activeTheme prop', () => {
+    // create wrapped component with 'theme1' set as active theme
+    const RootWithThemes = injectThemes(themes, 'theme1')(RootComponent)
+    const component = shallow(<RootWithThemes />)
+
+    expect(component.props().activeTheme).toBe('theme1')
+  })
+
+  it('changeTheme prop should be defined', () => {
+    const RootWithThemes = injectThemes(themes, 'theme1')(RootComponent)
+    const component = shallow(<RootWithThemes />)
+
+    expect(component.props().changeTheme).toBeDefined()
+  })
+
+  it('should allow prop passthrough', () => {
+    const RootWithThemes = injectThemes(themes, 'theme1')(RootComponent)
+    // pass in a custom prop to the wrapped component to see if it passes through properly
+    const component = shallow(<RootWithThemes otherProp="prop" />)
+
+    expect(component.props().otherProp).toBe('prop')
+  })
+})
